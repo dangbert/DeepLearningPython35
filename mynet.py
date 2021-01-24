Fix slowdown in mynet.py:backprop() now using matrix form of all equations instead of looping over nodes in each layer.  Finished unit test for SGD(). mynet.py is now working as desired (as of end of chapter 2).

mynet.py
@@ -67,35 +67,36 @@ class Network(object):
                         for i in range(self.num_layers-1)]
         # TODO: network still trains slower than orignal, why? (1 epoch should get close to 90% accuracy)
 
-    #@staticmethod
     def save(self, filename):
         """
-        pickle data in this class as a backup
+        pickle data in this class as a backup to desired filename
         https://stackoverflow.com/a/2842727
-        Incomplete: take the given net (not self) and pickle its contents into a file
         """
         if os.path.exists(filename):
           os.remove(filename)
         f = open(filename, 'wb')
-        #pickle.dump(note.self.__dict__, f, 2)
         pickle.dump(self.__dict__, f, 2)
         f.close()
         print("*** network saved to '" + filename + "' ***")
 
-    #@staticmethod
     def load(self, filename):
         """
-        load data from pickle file to intialize it as a network
+        load data from pickle file into this class to overwrite its data
         Incomplete: (returns a Network object)
         """
-        # dummy-weights for initaliztion (will be overwritten by pickel file anyway)
-        tmp =  Network([784, 30, 10])
         f = open(filename, 'rb')
-        tmp.__dict__.update(pickle.load(f))
+        self.__dict__.update(pickle.load(f))
         f.close()
-        #return tmp # return new instance of a Network
-        #print("*** network loaded from '" + filename + "' ***")
+        print("*** network loaded from '" + filename + "' ***")
 
+    def test(self, test_data):
+        """Return the number of test inputs for which the neural
+        network outputs the correct result. Note that the neural
+        network's output is assumed to be the index of whichever
+        neuron in the final layer has the highest activation."""
+        test_results = [(np.argmax(self.getOutput(x)), y)
+                        for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
 
     def getOutput(self, x):
         """
@@ -115,8 +116,11 @@ class Network(object):
             activations.append(self.sigmoid(zs[-1]))
         return zs, activations
 
-    # TODO: there's a way to more efficiently do this
-    # (combining all data in the batch into a single array...)
+    # TODO: there's a way to more efficiently do this!
+    #   (combining all data in the batch into a single matrix, as the book mentions)
+    #   (takes advantage of parallelization in CPU and GPU's)
+    #   try to work this out myself but if needed reference:
+    #   https://medium.com/@hindsellouk13/matrix-based-back-propagation-fe143ce2b2df
     def updateMiniBatch(self, miniBatch, rate):
         """
         Perform gradient descent using backpropogation on a single miniBatch
@@ -166,7 +170,8 @@ class Network(object):
         if test_data:
             test_data = list(test_data)
             n_test = len(test_data)
-            print("INITIAL: {} / {}".format(self.test(test_data), n_test))
+            n_correct = self.test(test_data)
+            print("INITIAL: {:.2f}% -- {} / {}".format((n_correct/n_test)*100, n_correct, n_test))
 
         for i in range(start_epoch, epochs+1):
             self.epoch += 1
@@ -181,71 +186,55 @@ class Network(object):
                 self.updateMiniBatch(mini_batch, rate)
                 batchNum += 1
             if test_data:
-                print("Epoch {} : {} / {}".format(i, self.test(test_data), n_test))
+                n_correct = self.test(test_data)
+                print("Epoch {}: {:.2f}% -- {} / {}".format(i, (n_correct/n_test*100), n_correct, n_test))
             else:
                 print("Epoch {} complete".format(i))
 
-            if i % 5 == 0:
+            if i % 10 == 0:
                 self.save(backup_dir + "/latest.pkl")
             if i % 25 == 0:
                 self.save(backup_dir + "/epoch" + str(i) + ".pkl")
 
         self.save(backup_dir + "/epoch" + str(epochs) + ".pkl")
+        self.save(backup_dir + "/latest.pkl")
         print("\ntraining complete (reached epoch" + str(epochs) + ")")
 
-    def test(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.getOutput(x)), y)
-                        for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
-
-    # do backpropogation
-    # returns nabla_b, nabla_w
-    # (partial dertivatives of biases and weights wrt. cost function)
-    def backprop(self, cur, expected):
-        # TODO: also use zs from this function?
-        zs, activations = self.feedforward(cur)
-
+    def backprop(self, x, y):
+        """
+        do backpropagation
+        returns nabla_b, nabla_w (partial dertivatives of biases and weights wrt. cost function)
+        """
+        zs, activations = self.feedforward(x)
         # note: nabla_b is the same as the "error" calculated at each node
         nabla_b = [np.zeros(b.shape, dtype=float) for b in self.biases]
         nabla_w = [np.zeros(w.shape, dtype=float) for w in self.weights]
+        # array to store the calculated error at each node as we backpropagate
+        errors = [ np.zeros((n, 1), dtype=float) for n in self.sizes[1:] ]
 
-        # array to store the calculated error at each node as we backpropogate
-        errors = [np.zeros((n, 1), dtype=float) for n in self.sizes]
-        # traverse layers backwards
-        # TODO: consider adding a dummy layer of to the beginning of the weights and biases vectors
-        #       so the indicies make sense and match up with the errors array, etc
-        for l in range(len(self.sizes)-1, 0, -1):
-            index = l-1 # notes: subtract 1 from layer number to index into weights and biases arrays
-            # traverse nodes in layer l
-            for j in range(self.sizes[l]):
-                # calculate errors[l][j]
-                al_j = activations[l][j]
-                # if we're on the last layer
-                if l == len(self.sizes)-1:  # this is the equivalent of me calculating delta
-                    # TODO: use sigmoid_prime and cost_derivative directly here
-                    errors[l][j] = (al_j-expected[j]) * al_j*(1-al_j) # BP1
-                else:
-                    # BP2
-                    total = 0
-                    for k in range(0, self.sizes[l+1]):
-                        total += self.weights[index+1][k][j] * errors[l+1][k][0]
-                    errors[l][j] = al_j*(1-al_j) * total
-
-                nabla_b[index][j] = errors[l][j]
-                for k in range(0, self.sizes[l-1]):
-                    nabla_w[index][j][k] = errors[l][j]*activations[l-1][k]
+        # using the index -l we will traverse the layers backwards
+        #   (convenient for indexing into weights and biases arrays)
+        for l in range(1, len(self.sizes)):
+            # NOTE: using the matrix forms of the equations instead of looping over the nodes in the
+            #   current layer is way faster (50x difference in one test)
+            if l == 1: # BP1:
+                errors[-l] = self.cost_derivative(activations[-l], y) * self.sigmoid_prime(zs[-l])
+            else:      # BP2:
+                # (np.multiply() does the hadmard product, np.dot() does matrix multiplication)
+                errors[-l] = np.multiply( np.dot(self.weights[-l+1].transpose(),errors[-l+1]),  self.sigmoid_prime(zs[-l]) )
+            nabla_b[-l] = errors[-l] # BP3
+            nabla_w[-l] = np.dot(errors[-l], activations[-l-1].transpose()) # BP4 (a matrix form)
         return (nabla_b, nabla_w)
 
-    # returns the vector of partial derivatives for
-    # the cost with respect to output activations
     def cost_derivative(self, output_activations, y):
         r"""
         Return the vector of partial derivatives $\partial C_x / \partial a$
         for the output activations.
+        (For the quadratic cost function $C$ in particular)
+
+        Args:
+            output_activations: vector of output activations
+            y: expected output vector
         """
         return (output_activations-y)
 

test1.py
@@ -6,6 +6,7 @@ import mnist_loader
 import os
 import glob
 import numpy as np
+import shutil
 
 # ---------------------
 # test network.py
@@ -14,12 +15,11 @@ import mynet as network
 
 def main():
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-    #visualizeResults(test_data)
-
     train()
+    visualizeResults(test_data)
     #test()
 
-# visualize results by outputing them to a file
+# visualize results by outputing them as images to a file
 # dataset = list of (x,y) pairs
 # fname = path to pkl file
 # saveSucccesses (if we should save correct predictions to images as well)
@@ -30,6 +30,10 @@ def visualizeResults(dataSet, fname="backups/latest.pkl", saveSuccesses=False):
     print("testing provided dataset on network " + fname)
     index = 0
     correct = 0
+
+    if os.path.exists("results/"):
+        shutil.rmtree("results/")
+
     for (x, y) in dataSet:
         result = np.argmax(net.getOutput(x))
         status = "passed" if result==y else "failed"
@@ -51,24 +55,23 @@ def train():
     #print("Validation data: {} items".format(len(validation_data)))
     #print("Test data:\t {} items".format(len(test_data)))
 
-    start_epoch = 1
-    total_epochs = 1000
-    rate = 3.0
-    mini_batch_size = 10
+    start_epoch, total_epochs = 1, 15
+    rate, mini_batch_size = 3.0, 10
 
     # train new network
-    #net = network.Network([784, 30, 10])
-    net = network.Network([784, 5, 10])
+    #net = network.Network([784, 30, 20, 10])   # about 95% accuracy (after 15 epoch)
+    #net = network.Network([784, 100, 30, 10])  # about 96% accuracy (after 40 epoch)
+    net = network.Network([784, 30, 10])        # about 95% accuracy (after 10 epochs)
     if start_epoch != 1:
         net.load("backups/epoch" + str(start_epoch) + ".pkl")
 
     # start training
-    print("TODO: figure out why this seems to run so much slower compared to (provided) test.py")
     net.SGD(training_data, total_epochs, mini_batch_size, rate, test_data=test_data, start_epoch=start_epoch)
 
 def test():
     # load dataset
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    test_data = list(test_data)
     training_data = list(training_data)
 
     # load old network from file and test
@@ -81,8 +84,9 @@ def test():
     for fname in files:
         net = network.Network([784, 30, 10])
         net.load(fname)
-        res = net.evaluate(list(test_data))
-        print("{}\t-> {} / {}".format(fname, res, len(list(test_data))))
+        n_test = len(test_data)
+        n_correct = net.test(test_data)
+        print("{}\t: {:.2f}% -- {} / {}".format(fname, (n_correct/n_test)*100, n_correct, n_test))
     
 if __name__ == "__main__":
     main()

tests/test_network1.py
@@ -13,7 +13,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import copy
 import numpy as np
 import random
+from timeit import default_timer as timer
 
+import mnist_loader
 import network
 import mynet
 
@@ -24,6 +26,7 @@ def test_feed_forward():
     """
     test that feedforward() behaves the same across the two implemenations
     """
+    myTotalSec, refTotalSec = 0, 0
     for sizes in [ [4,2,3], [1,1], [10-x for x in range(10)], [(x+3)*((x%2)+3) for x in range(10)], [500, 3, 400, 1000] ]: 
         #sizes = [4,2,3]
         net = network.Network(sizes)
@@ -38,12 +41,24 @@ def test_feed_forward():
             # my feedforward function additionally returns the Z value for each node
             assert(np.allclose(mine.sigmoid(zs[-1]), A[-1]))
             assert(np.allclose(mine.getOutput(x), A[-1]))
-            assert(np.allclose(net.feedforward(x), mine.getOutput(x)))
+
+            start = timer()
+            y_net = net.feedforward(x)
+            refTotalSec += timer() - start
+
+            start = timer()
+            y_mine = mine.getOutput(x)
+            myTotalSec += timer() - start
+
+            assert(np.allclose(y_net, y_mine))
+    print("reference: feedforward() cumulative time: {:.4f} sec".format(refTotalSec))
+    print("mine:      feedforward() cumulative time: {:.4f} sec\n\n".format(myTotalSec))
 
 def test_backprop():
     """
     test that backprop() behaves the same across the two implemenations
     """
+    myTotalSec, refTotalSec = 0, 0
     for sizes in [ [4,2,3], [1,1], [10-x for x in range(10)], [(x+3)*((x%2)+3) for x in range(10)], [100, 50, 150] ]: 
         net = network.Network(sizes)
         # make a copy of this network using my class instead
@@ -53,26 +68,33 @@ def test_backprop():
         print("testing backprop function with network size: " + str(sizes))
         X, Y = get_dummy_training_data(sizes[0], sizes[-1], count=25)
         for x, y in zip(X, Y):
+            start = timer()
             nabla_b, nabla_w = net.backprop(x, y)
+            refTotalSec += timer() - start
+
+            start = timer()
             mine_b, mine_w = mine.backprop(x, y)
+            myTotalSec += timer() - start
             # https://stackoverflow.com/a/30773738
             assert(all([np.allclose(nb, mb) for nb, mb in zip(nabla_b, mine_b)]))
             assert(all([np.allclose(nw, mw) for nw, mw in zip(nabla_w, mine_w)]))
+    print("reference: backprop() cumulative time: {:.4f} sec".format(refTotalSec))
+    print("mine:      backprop() cumulative time: {:.4f} sec\n\n".format(myTotalSec))
 
 def test_update_mini_batch():
     """
     test that update_mini_batch() behaves the same across the two implemenations
     """
+    myTotalSec, refTotalSec = 0, 0
     for sizes in [ [4,2,3], [1,1], [10-x for x in range(8)], [(x+3)*((x%2)+3) for x in range(7)], [100, 50, 20] ]: 
         net = network.Network(sizes)
         # make a copy of this network using my class instead
         mine = mynet.Network(sizes)
         mine.weights, mine.biases, mine.sizes = copy.deepcopy(net.weights), copy.deepcopy(net.biases), copy.deepcopy(net.sizes)
 
-        print("testing update_mini_batch function with network size: " + str(sizes))
         X, Y = get_dummy_training_data(sizes[0], sizes[-1], count=25)
         batchSize = random.randint(1, 7)
-        print("\tusing batchSize = {}".format(batchSize))
+        print("testing update_mini_batch function with network size: {}, batchSize: {}".format(sizes, batchSize))
 
         while len(X) > 0:
             batch = [(x,y) for x,y in zip(X[:batchSize],Y[:batchSize])]
@@ -81,20 +103,52 @@ def test_update_mini_batch():
             iterations = 7 #random.randint(3, 10)
             eta = random.uniform(0.01, 0.25) # learning rate
             for i in range(iterations):
+                start = timer()
                 net.update_mini_batch(batch, eta)
+                refTotalSec += timer() - start
+
+                start = timer()
                 mine.updateMiniBatch(batch, eta)
+                myTotalSec += timer() - start
             # test weights and biases were updated to same values
-            assert(all([np.allclose(b, mb) for b, mb in zip(net.weights, mine.weights)]))
-            assert(all([np.allclose(w, mw) for w, mw in zip(net.biases, mine.biases)]))
+            assert(all([np.allclose(b, mb) for b, mb in zip(net.biases, mine.biases)]))
+            assert(all([np.allclose(w, mw) for w, mw in zip(net.weights, mine.weights)]))
 
         # also compare final outputs on some training data (after adjusting weights/biases)
         X, Y = get_dummy_training_data(sizes[0], sizes[-1], count=10)
         for x,y in zip(X, Y):
             assert(np.allclose(net.feedforward(x), mine.getOutput(x)))
+    print("reference: update_mini_batch() cumulative time: {:.4f} sec".format(refTotalSec))
+    print("mine:      update_mini_batch() cumulative time: {:.4f} sec\n\n".format(myTotalSec))
 
 def test_SGD():
     """
     test that SGD() behaves the same across the two implemenations
     """
-    # TODO: finish this
-    pass
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    training_data = list(training_data)[:100]
+    validation_data = list(validation_data)[:100]
+    test_data = list(test_data)[:100]
+
+    net = network.Network([784, 5, 10])
+    mine = mynet.Network([784, 5, 10])
+    mine.weights, mine.biases, mine.sizes = copy.deepcopy(net.weights), copy.deepcopy(net.biases), copy.deepcopy(net.sizes)
+
+    total_epochs, rate, mini_batch_size = 20, 3.0, 8
+    myTotalSec, refTotalSec = 0, 0
+    random.seed(0); np.random.seed # set random seed so behaviour is reproducible
+    start = timer()
+    net.SGD(training_data, epochs=total_epochs, mini_batch_size=mini_batch_size, eta=rate, test_data=test_data)
+    refTotalSec += timer() - start
+
+    random.seed(0); np.random.seed # ensure mini batches are shuffled the same as the last test
+    start = timer()
+    mine.SGD(training_data, epochs=total_epochs, mini_batch_size=mini_batch_size, rate=rate, test_data=test_data)
+    myTotalSec += timer() - start
+    print("reference: SGD took {:.4f} seconds".format(timer() - start))
+    print("mine:      SGD took {:.4f} seconds\n\n".format(timer() - start))
+
+    # test weights and biases were updated to same values
+    #import pdb; pdb.set_trace()
+    assert(all([np.allclose(b, mb) for b, mb in zip(net.biases, mine.biases)]))
+    assert(all([np.allclose(w, mw) for w, mw in zip(net.weights, mine.weights)]))